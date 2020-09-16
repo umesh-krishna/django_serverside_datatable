8cf811618c564f08e48728300ac94f35a57cad41
umesh krishna <umeshkrishna33@gmail.com>
2020-09-16 21:26:49 +0530

    restructured

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='django_serverside_datatable',
+setuptools.setup(
+      name='django_serverside_datatable',
       version='0.1.0',
       description="Simple Server-side Datatable processing view for Django",
-      url="https://github.com/umesh-krishna/django_serverside_datatables",
+      url="https://github.com/umesh-krishna/django_serverside_datatable",
       license="MIT",
       author="Umesh Krishna",
       author_email='umeshkrishna.ur@gmail.com',
-      packages=['django_server_side_datatable'],
+      packages=['django_serverside_datatable'],
       install_requires=['Django>=1.8']
-      )
+)
